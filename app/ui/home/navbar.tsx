9e3fb85147577328f17738442211cc598439c184
OwuorV <myvinmail591@gmail.com>
2025-06-23 14:32:18 +0300

    making options clickable temporarily

--- app/ui/home/navbar.tsx
+++ app/ui/home/navbar.tsx
@@ -29,17 +29,19 @@ export default function Navbar() {
             <Link href="/vets" className=" hidden md:flex">
               Cart
             </Link>
-            <span className="flex md:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="24px"
-                viewBox="0 -960 960 960"
-                width="24px"
-                fill="#000"
-              >
-                <path d="M360-200v-80h480v80H360Zm0-240v-80h480v80H360Zm0-240v-80h480v80H360ZM200-160q-33 0-56.5-23.5T120-240q0-33 23.5-56.5T200-320q33 0 56.5 23.5T280-240q0 33-23.5 56.5T200-160Zm0-240q-33 0-56.5-23.5T120-480q0-33 23.5-56.5T200-560q33 0 56.5 23.5T280-480q0 33-23.5 56.5T200-400Zm0-240q-33 0-56.5-23.5T120-720q0-33 23.5-56.5T200-800q33 0 56.5 23.5T280-720q0 33-23.5 56.5T200-640Z" />
-              </svg>
-            </span>
+            <Link href="/farms">
+              <span className="flex md:hidden">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="24px"
+                  viewBox="0 -960 960 960"
+                  width="24px"
+                  fill="#000"
+                >
+                  <path d="M360-200v-80h480v80H360Zm0-240v-80h480v80H360Zm0-240v-80h480v80H360ZM200-160q-33 0-56.5-23.5T120-240q0-33 23.5-56.5T200-320q33 0 56.5 23.5T280-240q0 33-23.5 56.5T200-160Zm0-240q-33 0-56.5-23.5T120-480q0-33 23.5-56.5T200-560q33 0 56.5 23.5T280-480q0 33-23.5 56.5T200-400Zm0-240q-33 0-56.5-23.5T120-720q0-33 23.5-56.5T200-800q33 0 56.5 23.5T280-720q0 33-23.5 56.5T200-640Z" />
+                </svg>
+              </span>
+            </Link>
             <div className="bg-green-300 rounded-[20px] w-[40px] overflow-hidden h-[40px]">
               <Image
                 src={"/me.png"}
