c104927a84e42f443ef46b0b9bfdd46e0000f128
OwuorV <myvinmail591@gmail.com>
2025-06-24 17:10:07 +0300

    changing z index of toggle bar

--- app/ui/dashboard/dashboardMain.tsx
+++ app/ui/dashboard/dashboardMain.tsx
@@ -18,6 +18,7 @@ export default function DashboardMain({ toggle, children }: DashboardnavProps) {
             viewBox="0 -960 960 960"
             width="24px"
             fill="#00000088"
+            z={1000}
           >
             <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm280-80h280v-560H480v560Z" />
           </svg>
