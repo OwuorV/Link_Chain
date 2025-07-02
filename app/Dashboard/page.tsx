d292ba99de16e21e6018ed837c10de747c117540
OwuorV <myvinmail591@gmail.com>
2025-07-02 19:52:19 +0300

    dashboard

--- app/Dashboard/page.tsx
+++ app/Dashboard/page.tsx
@@ -9,7 +9,7 @@ import QuickActions from "../ui/dashboard/quickActions";
 export default function MainLayout() {
   return (
     <div className="flex flex-col w-full gap-10 mt-10 px-2 md:px-10">
-      <div className="text-[32px] font-bold">Welcome to your Dashborad</div>
+      <div className="text-[32px] font-bold">Welcome to your Dashboard</div>
       <div className="flex flex-col md:flex-row gap-8">
         <div className=" w-full md:w-[60%] h-max md:h-[220px] card1 bg-[#A9DFD8]/20 text-[#171821] rounded-[13px] px-[14px] py-[19px] flex flex-col justify-between shadow-sm">
           <div className="header">
